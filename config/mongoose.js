8c5e5f764cdf3f66aececa8ae2f7677ab2593921
Chomal Miguntenne <miguntenne@gmail.com>
2016-12-26 21:02:23 +0530

    changed mongodb uri

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@
 * Has db URI and uses mongoose to connect to the path.
 */
 var mongoose = require('mongoose');
-var dbURI = 'mongodb://localhost/angularSimpleDemo';
+var MONGOLAB_GOLD_URI = 'mongodb://heroku_scdpdml4:l7va8t57di69b91mjrqni4hvk4@ds145168.mlab.com:45168/heroku_scdpdml4';
 
 mongoose.connect(dbURI);
 
